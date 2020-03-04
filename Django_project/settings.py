35be443cc3ca7319c581c4b57b20112b0a97ff08
abdomashael <abdomashael@yahoo.com>
2020-03-04 22:44:58 +0200

    add database config

--- Django_project/settings.py
+++ Django_project/settings.py
@@ -79,10 +79,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'dokana',
-        'USER':'root',
-        'PASSWORD':'',
+        'USER':'dokana_admin',
+        'PASSWORD':'123456789',
         'HOST':'127.0.0.1',
-        'PORT':'3307'
+        'PORT':'3306'
     }
 }
 
