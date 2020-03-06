37e33c8bc46a0c9cc35a21260220225248f37e34
abdomashael <abdomashael@yahoo.com>
2020-03-06 23:21:41 +0200

    add address model class

--- Dokana/models.py
+++ Dokana/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from User.models import User
+
 
 # Create your models here.
+class Product(models.Model):
+    productID = models.IntegerField(primary_key=True)
+
+
+class Reviews(models.Model):
+    productID = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    userID = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    review = models.TextField(max_length=1000)
+
+    class Meta:
+        unique_together = ('productID', 'userID')
