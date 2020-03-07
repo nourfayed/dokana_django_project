810018f82b7032983ebaef4ee5e12eb44a6f225a
fadyjan <fady.jan95@gmail.com>
2020-03-07 23:35:31 +0200

    merge master branch with models

--- Cart/models.py
+++ Cart/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Cart(models.Model):
+    PAYMENT_TYPES = (
+        ('c', 'cash'),
+        ('v', 'visa')
+    )
+    paymentMethod = models.CharField(choices=PAYMENT_TYPES, max_length=50)
+    productID = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    userID = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = [['productID', 'userID']]
+
+    def __str__(self):
+        return self.productID
+
+    def removeFromCart(self, productID):
+        Cart.objects.get(self.productID == productID).delete()
+
+    def addProductToCart(self,paymentMethod , productID , userID):
+        self.paymentMethod = paymentMethod
+        self.productID = productID
+        self.userID = userID
+
+        Cart.save()
+
+    def getUserCart(self, userID):
+        return Cart.objects.get(self.userID == userID)
--- Dokana/models.py
+++ Dokana/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    categoryID = models.IntegerField(primary_key=True, max_length=10)
+    categoryName = models.TextField(max_length=20)
+
+    def __str__(self):
+        return self.categoryID
+
+    def addCategory(self, categoryID, categoryName):
+        self.categoryID = categoryID
+        self.categoryName = categoryName
+        Category.save()
+
+    def deleteCategory(self, categoryID):
+        Category.objects.get(self.categoryID == categoryID).delete()
+
+    def modifyCategoryName(self, newName):
+        self.categoryName = newName
+        Category.save()
