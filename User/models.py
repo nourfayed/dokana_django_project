62f27c5bb3180631ce78919aed25762a9ae53ae5
abdomashael <abdomashael@yahoo.com>
2020-03-07 23:29:53 +0200

    add delete address method

--- User/models.py
+++ User/models.py
@@ -8,12 +8,18 @@ class User(models.Model):
 
 class Address(models.Model):
     userID = models.ForeignKey(to=User, on_delete=models.CASCADE)
-    address = models.TextField()
+    address = models.TextField(max_length=500)
 
     def __str__(self):
         return "UserId : " + self.userID + "Address :" + self.address
 
-    def addAddress(self):
+    def addAddress(self, address):
+        add = Address()
+        add.userID = address.userID
+        add.address = address.address
+        add.save()
+
+    def deleteAddress(self,):
         pass
 
     class Meta:
