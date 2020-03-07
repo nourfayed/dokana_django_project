868084839fd3b4b99b41f2533e353f5b320097f3
abdomashael <abdomashael@yahoo.com>
2020-03-07 23:29:05 +0200

    add remove review method

--- Dokana/models.py
+++ Dokana/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models import Q
+
 from User.models import User
 
 
@@ -11,6 +13,18 @@ class Reviews(models.Model):
     productID = models.ForeignKey(to=Product, on_delete=models.CASCADE)
     userID = models.ForeignKey(to=User, on_delete=models.CASCADE)
     review = models.TextField(max_length=1000)
+
+    @classmethod
+    def addReview(cls, new_review):
+        review = Reviews()
+        review.userID = new_review.userID
+        review.productID = new_review.productID
+        review.review = new_review.review
+        review.save()
+
+    @classmethod
+    def removeReview(cls, userID, productID):
+        Reviews.objects.get(Q(userID=userID) & Q(productID=productID))
 
     class Meta:
         unique_together = ('productID', 'userID')
