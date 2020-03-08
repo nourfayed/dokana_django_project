ff153bd96df17c33af3961eaaeebc2a724cb95ff
Nour Fayed <nourfayedd@gmail.com>
2020-03-08 22:56:03 +0200

    add products views.

--- Django_project/urls.py
+++ Django_project/urls.py
@@ -1,4 +1,4 @@
-"""Django_project URL Configuration
+"""dokana URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls.static import static
+from django.urls import path ,include
+
+
+from dokana import settings
 
 urlpatterns = [
+    path('products/', include('products.urls')),
     path('admin/', admin.site.urls),
-]
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
