154f835a417503d08ebfd28ebbe092c0f281e018
fadyjan <fady.jan95@gmail.com>
2020-03-12 19:39:48 +0200

    Cart home desgin with payments methods and listing products

--- Cart/admin.py
+++ Cart/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Cart, Product, User
+
+admin.site.register(Cart)
+admin.site.register(Product)
+admin.site.register(User)
