5e101069feccecefdb862c6f18f9e8158cb49432
nourfayed <nourfayedd@gmail.com>
2020-03-15 23:59:48 +0200

    add reviews to products

--- products/views.py
+++ products/views.py
@@ -1,23 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Category, Products
+from .forms import ReviewForm
+from User.models import User
 
 
 def index(request):
     products = Products()
     latest_products = products.getAllProducts()
-    print("w eh kaman")
-    print(latest_products)
     data = {'latest_products': latest_products}
     return render(request, 'products/index.html', data)
 
 
 def showDetails(request):
     currentProductId = request.POST.get('productID', '')
-
-    print("current product id----------------------")
+    print("Current product id from show details................................")
     print(currentProductId)
     product = Products.objects.get(productID=currentProductId)
-    print("mn gowa sho details")
-    print(product.productID)
-    data = {'product': product}
+
+    form = ReviewForm(request.POST)
+    if form.is_valid():
+        review = form.save(commit=False)
+        review.productID = product
+        user = User()
+        user.userId=1
+        user.userName = "7amada"
+        user.email = "fgklhkk@gmail.com"
+        user.password = "12435465rydf"
+        user.phone =1234546
+        user.save()
+        review.userID = user
+        review.save()
+    data = {'product': product, 'form': form}
     return render(request, "products/productDetails.html", data)
+
+
