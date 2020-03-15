93a78797699e8c995e5d03c0e88f8d585d816ef8
abdomashael <abdomashael@yahoo.com>
2020-03-15 23:59:09 +0200

    reg reviews to admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Category, Products, SubCategory
+from .models import Category, Products, SubCategory, Reviews
 
 admin.site.register(Category)
 admin.site.register(Products)
 admin.site.register(SubCategory)
+admin.site.register(Reviews)
