25c2991a153d2a59e1da2a59ff8e72ebef4daffe
nourfayed <nourfayedd@gmail.com>
2020-03-16 00:51:25 +0200

    add reviews and product details.

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('productDetails.html', views.showDetails)
+    path('productDetails.html', views.showDetails),
 
     path('', views.index, name='index'),
     path('search/',views.search,name="search")
