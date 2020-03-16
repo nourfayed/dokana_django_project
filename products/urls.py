0964cda77d6df79af36750c8eafaaa8d8b3cd53f
abdomashael <abdomashael@yahoo.com>
2020-03-16 16:46:58 +0200

    search results card view add btn "addTo Card"

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path('', views.index),
     path('productDetails/', views.showDetails),
-    path('search/', views.search, name="search")
+    path('search/', views.search, name="search"),
+    path('cart/', include('Cart.urls'))
 ]
