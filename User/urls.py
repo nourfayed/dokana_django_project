afe6a786bf47bef9a68ba689a30a4d060f1f8119
hema <hema@localhost.localdomain>
2020-03-18 19:26:15 +0200

    update user deactivated feture

--- User/urls.py
+++ User/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     path('<int:pk>/history/',views.history, name='History'),
     path('<int:pk>/changePassword/', views.changePass, name='Change Password'),
     path('<int:pk>/deactivate/', views.delete_profile, name='Delete profile'),
+ 
     path('<int:pk>/logout/', views.logout, name='logout'),
+ 
+
+
+ 
 ]
