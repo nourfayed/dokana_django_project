133d05aedb369e274f4065cdd9a043aa59d4040b
abdomashael <abdomashael@yahoo.com>
2020-03-18 18:11:00 +0200

    add deactivate to urls

--- User/urls.py
+++ User/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('<int:pk>/', views.profile, name='profile'),
     path('<int:pk>/history/',views.history, name='History'),
     path('<int:pk>/changePassword/', views.changePass, name='Change Password'),
+    path('<int:pk>/deactivate/', views.delete_profile, name='Delete profile'),
 
 
 ]
